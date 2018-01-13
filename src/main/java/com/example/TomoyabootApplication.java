6fd3fc930f6ec7a5615b93f801b76076025164f6
tomoya <tomoya0128@gmail.com>
2018-01-13 14:48:12 +0900

    初回コミット

---
+++ src/main/java/com/example/TomoyabootApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomoyabootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomoyabootApplication.class, args);
+	}
+}
